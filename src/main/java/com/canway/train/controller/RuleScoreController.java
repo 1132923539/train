38fcaf5cf858e838f8c704143e50500a9e72e1ac
eltons <elton@canway.net>
2018-08-29 21:05:14 +0800

    修改

---
+++ src/main/java/com/canway/train/controller/RuleScoreController.java
@@ -0,0 +1,7 @@
+package com.canway.train.controller;
+
+/**
+ * @author eltons,  Date on 2018-08-29.
+ */
+public class RuleScoreController {
+}
--- src/main/java/com/canway/train/service/impl/RuleScoreServiceImpl.java
+++ src/main/java/com/canway/train/service/impl/RuleScoreServiceImpl.java
@@ -1,9 +1,11 @@
 package com.canway.train.service.impl;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import com.canway.train.bean.RuleScoreVO;
 import com.canway.train.entity.RuleScoreDO;
 import com.canway.train.mapper.RuleScoreMapper;
 import com.canway.train.service.RuleScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +16,10 @@ import java.util.List;
 @Service
 public class RuleScoreServiceImpl extends BaseServiceImpl<RuleScoreMapper, RuleScoreDO> implements RuleScoreService {
 
+
     @Override
     public List<RuleScoreVO> listAllRuleScore(Long trainingId) {
+//        this.selectPage(Page < >)
         return null;
     }
 
