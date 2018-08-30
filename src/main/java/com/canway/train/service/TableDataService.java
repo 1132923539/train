c4e8bc4007031bdf36b42b9d7c9b76eeff2f29c2
asfanty <875199251@qq.com>
2018-08-30 17:26:34 +0800

    增加结果列表接口

--- src/main/java/com/canway/train/service/TableDataService.java
+++ src/main/java/com/canway/train/service/TableDataService.java
@@ -1,10 +1,16 @@
 package com.canway.train.service;
 
+import com.canway.train.bean.RankVO;
+
+import java.util.List;
+import java.util.Map;
+
 public interface TableDataService  {
 
-    public void getSpeechRank();
+    public Map<Long,RankVO> getGroup(Long trainingId);
 
-    public int getRuleScore(Boolean isAdd);
+    public List<RankVO> getSpeechTotal(Map<Long,RankVO> groupVOMap, Long trainingId);
 
-    public void getTotalRanking();
+    public List<RankVO> solveRuleScore(List<RankVO> rankVOS,Long trainingId);
+
 }
